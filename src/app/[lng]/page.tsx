427040e58be50bd854678e9e01f98e5a5fcf21f8
qwerty0zero <igoricloud98@gmail.com>
2026-05-11 23:55:40 +0200

    refactor: remove theme toggle

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -5,18 +5,14 @@ export default async function Page() {
   const { t } = await getT()
 
   return (
-    <div className="p-6 flex min-h-svh">
-      <div className="max-w-md min-w-0 gap-4 text-sm leading-loose flex flex-col">
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">{t("title")}</h1>
           <p>{t("description")}</p>
           <p>{t("button_desc")}</p>
           <Button className="mt-2">{t("button")}</Button>
         </div>
-        <div
-          className="font-mono text-xs text-muted-foreground"
-          dangerouslySetInnerHTML={{ __html: t("theme_toggle") }}
-        />
       </div>
     </div>
   )
