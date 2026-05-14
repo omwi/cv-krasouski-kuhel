53af070df292e19049e2a207fae6010c4904223a
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-05-14 12:16:26 +0200

    feat: change button styles, rename to kebab-case, remove unused variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,24 +5,20 @@ import { Slot } from "radix-ui"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "group/button min-w-55 inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:text-muted-foreground disabled:shadow-transparent aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "group/button inline-flex min-w-55 shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:text-muted-foreground disabled:shadow-transparent aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-elevation-2 disabled:bg-muted [a]:hover:bg-primary/80",
+          "bg-primary text-primary-foreground shadow-elevation-2 hover:bg-primary/70 disabled:bg-muted",
         outline:
-          "dark:border-outline-btn border-border bg-transparent text-outline-foreground hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-input/30 dark:hover:bg-input/50",
-        outlinePrimary:
+          "border-border bg-transparent text-outline-foreground hover:bg-muted/50 aria-expanded:bg-muted aria-expanded:text-foreground",
+        "outline-primary":
           "border-primary bg-transparent text-primary hover:bg-primary/20 aria-expanded:bg-muted aria-expanded:text-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80 disabled:bg-muted aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
         ghost:
-          "text-neutral-500 hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
-        ghostPrimary:
+          "text-neutral-500 hover:bg-muted/50 aria-expanded:bg-muted aria-expanded:text-foreground",
+        "ghost-primary":
           "text-primary hover:bg-primary/20 aria-expanded:bg-muted aria-expanded:text-foreground",
-        destructive:
-          "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
