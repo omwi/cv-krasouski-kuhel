cf14b15fa185d3b5d0e93053de5487a53b381bf1
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-05-14 12:17:44 +0200

    feat: add min-w to close btn

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -71,7 +71,7 @@ function DialogContent({
           <DialogPrimitive.Close data-slot="dialog-close" asChild>
             <Button
               variant="ghost"
-              className="absolute top-2 right-2"
+              className="w-min-0 absolute top-2 right-2"
               size="icon-sm"
             >
               <XIcon />
