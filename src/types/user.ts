e7777356ce0f3515181446ee6d5a88704457252e
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-05-19 17:27:01 +0200

    feat: wip

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,11 @@
 export type User = {
   id: string
   email: string
-  role: string
+  role?: string
   avatarSrc: string | null
-  fullName: string
+  fullName?: string
+  fishName?: string
+  lastName?: string
   departmentName: string
   positionName: string
 }
