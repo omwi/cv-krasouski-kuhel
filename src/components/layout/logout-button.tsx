1e763c3023fbf8ae5d1ab2ecf1f1a856ef4677c9
omwi <bsuiromwi@gmail.com>
2026-05-19 14:36:37 +0300

    feat: use useLogout

--- src/components/layout/logout-button.tsx
+++ src/components/layout/logout-button.tsx
@@ -1,18 +1,18 @@
 import LogoutIcon from "@mui/icons-material/Logout"
 import { useT } from "next-i18next/client"
 
+import { useLogout } from "@/features/auth/hooks/use-logout"
+
 import { Button } from "../ui/button"
 
 export default function LogoutButton() {
   const { t } = useT("nav")
 
-  const handleClick = () => {
-    // todo: logout
-  }
+  const { logout } = useLogout()
 
   return (
     <Button
-      onClick={handleClick}
+      onClick={logout}
       variant={"ghost"}
       className="h-fit min-w-0 justify-start gap-2 rounded-none border-none px-4 py-1.5 font-normal text-foreground"
     >
