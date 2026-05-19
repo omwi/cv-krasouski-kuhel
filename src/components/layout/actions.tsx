aebc7afda59098f0c511ca0db10936f009bde5a8
omwi <bsuiromwi@gmail.com>
2026-05-19 15:39:04 +0300

    feat: replace mock userId in profile link

--- src/components/layout/actions.tsx
+++ src/components/layout/actions.tsx
@@ -1,26 +1,25 @@
+import { useReactiveVar } from "@apollo/client/react"
 import AccountCircleIcon from "@mui/icons-material/AccountCircle"
 import SettingsIcon from "@mui/icons-material/Settings"
 import { useT } from "next-i18next/client"
 
 import { paths } from "@/config/paths"
+import { authUserVar } from "@/lib/apollo/auth-var"
 
 import { Separator } from "../ui/separator"
 import ActionLink from "./action-link"
 import LogoutButton from "./logout-button"
 
-const user = {
-  id: 1,
-}
-
 export default function Actions() {
-  const { id } = user
-
   const { t } = useT("nav")
+  const user = useReactiveVar(authUserVar)
+
+  const userId = parseInt(user?.id ?? "0")
 
   return (
     <div className="flex flex-col gap-2 py-2">
       <nav className="flex flex-col">
-        <ActionLink to={paths.users.profile.get(id)}>
+        <ActionLink to={paths.users.profile.get(userId)}>
           <AccountCircleIcon />
           {t("profile")}
         </ActionLink>
