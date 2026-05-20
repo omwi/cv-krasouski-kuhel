e7b9c70fda8df902b801214690ed5c2f1ac307fa
KrasovskiiVladislav <62138349+omwi@users.noreply.github.com>
2026-05-20 14:45:59 +0300

    fix: tab nav (#61)
    * fix: tab nav
    
    * refactor: rename type

--- src/app/(pages)/auth/layout.tsx
+++ src/app/(pages)/auth/layout.tsx
@@ -1,15 +1,10 @@
-import TabHeader from "@/components/layout/tab-header"
+import TabNav, { TabLink } from "@/components/layout/tab-nav"
 
 import "@/features/auth/style/auth.css"
 
 import { paths } from "@/config/paths"
 
-export type HeaderLink = {
-  href: string
-  i18nKey: string
-}
-
-const LINKS: HeaderLink[] = [
+const LINKS: TabLink[] = [
   { href: paths.auth.login.get(), i18nKey: "header.login" },
   { href: paths.auth.signup.get(), i18nKey: "header.signup" },
 ]
@@ -20,7 +15,10 @@ export default function AuthLayout({
 }) {
   return (
     <>
-      <TabHeader i18nNamespace="auth" links={LINKS} />
+      <header>
+        <TabNav i18nNamespace="auth" links={LINKS} className="justify-center" />
+      </header>
+
       <section className="auth-container flex h-dvh w-full items-center justify-center">
         {children}
       </section>
