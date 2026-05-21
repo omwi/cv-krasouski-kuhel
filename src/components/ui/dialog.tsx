079d8703becc16d389080d9cc8848dfceb060943
qwerty0zero <igoricloud98@gmail.com>
2026-05-21 21:45:37 +0200

    refactor: update styles

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -106,7 +106,7 @@ function DialogFooter({
     <div
       data-slot="dialog-footer"
       className={cn(
-        "-mx-4 -mb-4 flex flex-col-reverse gap-2 rounded-b-xl p-4 sm:flex-row sm:justify-end",
+        "flex flex-col-reverse gap-2 rounded-b-xl sm:flex-row sm:justify-end",
         className
       )}
       {...props}
