118679686aae023cff89cedba2ba47ffba60630d
omwi <bsuiromwi@gmail.com>
2026-05-21 22:40:14 +0300

    fix: use user in user crumb

--- src/components/layout/crumbs/crumb-user.tsx
+++ src/components/layout/crumbs/crumb-user.tsx
@@ -1,6 +1,8 @@
+import { useQuery } from "@apollo/client/react"
 import { Users } from "lucide-react"
 
 import Crumb from "@/components/layout/crumbs/crumb"
+import { GET_USER } from "@/features/users/graphql/users/queries"
 
 type Props = {
   userId: number
@@ -9,10 +11,8 @@ type Props = {
 }
 
 export default function CrumbUser({ userId, href, isPage }: Props) {
-  // todo: useUser
-  const fullName = "John Doe"
-
-  const label = fullName || userId
+  const { data } = useQuery(GET_USER, { variables: { userId } })
+  const label = data?.user?.profile?.full_name ?? userId
 
   return (
     <Crumb href={href} isPrimary={true} isPage={isPage}>
