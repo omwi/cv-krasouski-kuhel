8c5136ddffb657a4fc67eedda5dcfc7834b86dee
omwi <bsuiromwi@gmail.com>
2026-05-21 12:21:14 +0300

    feat: add profile layout

---
+++ src/app/(pages)/(main)/users/[userId]/layout.tsx
@@ -0,0 +1,24 @@
+import TabNav, { TabLink } from "@/components/layout/tab-nav/tab-nav"
+import { paths } from "@/config/paths"
+
+type Props = {
+  children: React.ReactNode
+  params: Promise<{ userId: string }>
+}
+
+export default async function ProfileLayout({ children, params }: Props) {
+  const userId = parseInt((await params).userId)
+
+  const links: TabLink[] = [
+    { href: paths.users.details.get(userId), i18nKey: "profile" },
+    { href: paths.users.skills.get(userId), i18nKey: "skills" },
+    { href: paths.users.languages.get(userId), i18nKey: "languages" },
+  ]
+
+  return (
+    <div className="flex flex-col gap-4">
+      <TabNav links={links} i18nNamespace="nav" />
+      {children}
+    </div>
+  )
+}
