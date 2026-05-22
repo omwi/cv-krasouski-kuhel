fd1a995febecde9d2ac26c6c7632743373036c10
omwi <bsuiromwi@gmail.com>
2026-05-22 19:23:56 +0300

    chore: use absolute imports

--- src/features/users/components/profile/avatar-upload.tsx
+++ src/features/users/components/profile/avatar-upload.tsx
@@ -7,10 +7,9 @@ import { useT } from "next-i18next/client"
 import IconButton from "@/components/shared/icon-button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { FileUpload, FileUploadDropzone } from "@/components/ui/file-upload"
+import { useAvatarUpload } from "@/features/users/hooks/use-avatar-upload"
 import { GET_USER } from "@/graphql/users/queries"
 import { cn } from "@/lib/utils"
-
-import { useAvatarUpload } from "../../hooks/use-avatar-upload"
 
 export default function AvatarUpload({
   userId,
