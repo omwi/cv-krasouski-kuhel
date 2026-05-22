8cda3baeb0318406705eb6bd2d2196915607f446
omwi <bsuiromwi@gmail.com>
2026-05-22 10:41:29 +0300

    fix: main layout

--- src/app/(pages)/(main)/layout.tsx
+++ src/app/(pages)/(main)/layout.tsx
@@ -9,11 +9,11 @@ export default function MainLayout({ children }: Props) {
   return (
     <div className="flex h-dvh flex-col-reverse md:flex-row">
       <SideNav />
-      <div className="flex flex-1 flex-col overflow-hidden px-6 pt-4">
+      <div className="flex flex-1 flex-col overflow-auto px-6 pt-4">
         <header>
           <CrumbsNav />
         </header>
-        <main className="flex flex-1 flex-col overflow-hidden">{children}</main>
+        <main className="flex flex-1 flex-col">{children}</main>
       </div>
     </div>
   )
