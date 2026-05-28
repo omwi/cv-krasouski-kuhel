bfcf70be1db3753cdc357a123440f1e855dca878
omwi <bsuiromwi@gmail.com>
2026-05-28 10:40:00 +0300

    feat: add metadata

--- src/app/(pages)/(main)/cvs/page.tsx
+++ src/app/(pages)/(main)/cvs/page.tsx
@@ -1,3 +1,13 @@
+import { getT } from "next-i18next/server"
+
+export async function generateMetadata() {
+  const { t } = await getT("metadata")
+  return {
+    title: t("cvs.title"),
+    description: t("cvs.description"),
+  }
+}
+
 export default function Cvs() {
   return <div>Cvs</div>
 }
