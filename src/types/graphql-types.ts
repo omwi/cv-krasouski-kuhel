a6bf30519b61f84a4c16aff912640fcf0487e935
omwi <bsuiromwi@gmail.com>
2026-05-29 16:20:41 +0300

    fix: rebase

--- src/types/graphql-types.ts
+++ src/types/graphql-types.ts
@@ -1,5 +1,6 @@
 import {
   BaseCvFragment,
+  GetCvSkillsQuery,
   GetLanguagesQuery,
   GetUserLanguagesQuery,
   GetUserSkillsQuery,
@@ -15,3 +16,12 @@ export type UserLanguage = GetUserLanguagesQuery["profile"]["languages"][number]
 export type Language = GetLanguagesQuery["languages"][number]
 
 export type Cv = BaseCvFragment
+
+export type CvUserId = {
+  id: string
+  user: {
+    id: string
+  } | null
+}
+
+export type CvSkill = GetCvSkillsQuery["cv"]["skills"][number]
