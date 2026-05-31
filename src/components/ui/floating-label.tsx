003d12ceb74b0016d41697226a874c52554172c0
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-05-31 18:48:10 +0200

    fix:  floating-label focus (#117)
    fix: floating-label focus
    
    Co-authored-by: qwerty0zero <igoricloud98@gmail.com>

--- src/components/ui/floating-label.tsx
+++ src/components/ui/floating-label.tsx
@@ -18,7 +18,7 @@ export function FloatingLabel({
     <Label
       ref={ref}
       className={cn(
-        "peer-has-focus:primary pointer-events-none absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text bg-background px-2 text-sm text-input duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-hover:text-foreground peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
+        "pointer-events-none absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text bg-background px-2 text-sm text-input duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-hover:text-foreground peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus-visible:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
         className
       )}
       {...props}
