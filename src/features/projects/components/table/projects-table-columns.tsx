e6dfeafbdec1b0cece241d6c80d703bd85f7327b
qwerty0zero <igoricloud98@gmail.com>
2026-06-01 02:53:59 +0200

    fix: localize project dates

--- src/features/projects/components/table/projects-table-columns.tsx
+++ src/features/projects/components/table/projects-table-columns.tsx
@@ -1,9 +1,27 @@
+import { useT } from "next-i18next/client"
+
 import { TableColumnConfig } from "@/components/shared/data-table/data-table"
 import ProjectsRowActions from "@/features/projects/components/table/projects-row-actions"
 import { GetProjectsQuery } from "@/types/__generated__/graphql"
+import { parseUtcToLocal, toHumanDate } from "@/utils/date"
 import type { CurrentUser } from "@/utils/permissions"
 
 export type TableProjects = GetProjectsQuery["projects"][0]
+
+const DateCell = ({
+  dateStr,
+  isEndDate,
+}: {
+  dateStr?: string | null
+  isEndDate?: boolean
+}) => {
+  const { t, i18n } = useT(isEndDate ? "project-details" : "table")
+  const date = parseUtcToLocal(dateStr || undefined)
+  if (!date) {
+    return isEndDate ? <span className="text-sm">{t("till-now")}</span> : null
+  }
+  return <span className="text-sm">{toHumanDate(date, i18n.language)}</span>
+}
 
 export const getColumns = (
   currentUser: CurrentUser
@@ -31,16 +49,18 @@ export const getColumns = (
     titleKey: "projects-table.columns.start-date",
     sortable: true,
     searchable: false,
+    cell: ({ value }) => <DateCell dateStr={value as string} />,
   },
   {
     id: "end_date",
     titleKey: "projects-table.columns.end-date",
     sortable: true,
     searchable: false,
+    cell: ({ value }) => <DateCell dateStr={value as string} isEndDate />,
   },
   {
     id: "actions",
-    titleKey: "user-table.columns.actions",
+    titleKey: "projects-table.columns.actions",
     isSrOnly: true,
     sortable: false,
     searchable: false,
