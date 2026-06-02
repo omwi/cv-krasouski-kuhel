530ac8d12724948f81a593764a6b6ef5a3606fe7
omwi <bsuiromwi@gmail.com>
2026-06-02 11:47:46 +0300

    fix: scroll

--- src/app/[lng]/(main)/layout.tsx
+++ src/app/[lng]/(main)/layout.tsx
@@ -13,9 +13,7 @@ export default function MainLayout({ children }: Props) {
         <header>
           <CrumbsNav />
         </header>
-        <main className="flex min-w-0 flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
+        <main className="flex min-h-0 min-w-0 flex-1 flex-col">{children}</main>
       </div>
     </div>
   )
