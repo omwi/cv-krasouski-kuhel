21278b5092aae029e484d3aebd63c98b162de849
omwi <bsuiromwi@gmail.com>
2026-06-03 10:40:34 +0300

    refactor: cv layout

--- src/app/[lng]/(main)/cvs/[cvId]/layout.tsx
+++ src/app/[lng]/(main)/cvs/[cvId]/layout.tsx
@@ -17,9 +17,9 @@ export default async function ProfileLayout({ children, params }: Props) {
   ]
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex h-full flex-col gap-4">
       <TabNav links={links} i18nNamespace="nav" />
-      {children}
+      <div className="h-full overflow-y-auto">{children}</div>
     </div>
   )
 }
