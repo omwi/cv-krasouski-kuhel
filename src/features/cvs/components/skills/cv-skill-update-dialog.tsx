a5f45934c26400a075e7ca6b462561a3cf6a9d7e
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-06-03 16:57:42 +0200

    feat: refactor cv-skill-update-dialog

--- src/features/cvs/components/skills/cv-skill-update-dialog.tsx
+++ src/features/cvs/components/skills/cv-skill-update-dialog.tsx
@@ -1,19 +1,7 @@
 "use client"
 
-import { useT } from "next-i18next/client"
-import { Controller } from "react-hook-form"
-
-import { FormDialog } from "@/components/shared/dialog/form-dialog"
-import { Field } from "@/components/ui/field"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { SkillUpdateDialog } from "@/components/shared/skills/skill-update-dialog"
 import { useCvSkillUpdateForm } from "@/features/cvs/hooks/skills/use-update-cv-skill-form"
-import SkillMasterySelect from "@/features/skills/components/skill-mastery-select"
 import { CvSkill, CvUserId } from "@/types/graphql-types"
 
 type Props = {
@@ -27,45 +15,11 @@ export default function CvSkillUpdateDialog({
   cvUserId,
   cvSkill,
 }: Props) {
-  const { t } = useT(["buttons", "skills"])
-
-  const { control, onSubmit, isSubmitReady, open, setOpen } =
-    useCvSkillUpdateForm(cvUserId, cvSkill)
+  const formProps = useCvSkillUpdateForm(cvUserId, cvSkill)
 
   return (
-    <FormDialog
-      title={t("dialog.update", { ns: "skills" })}
-      submitLabel={t("confirm")}
-      cancelLabel={t("cancel")}
-      trigger={children}
-      open={open}
-      onOpenChange={setOpen}
-      onSubmit={onSubmit}
-      dialogClassName="w-150"
-      submitDisabled={!isSubmitReady}
-    >
-      <Field>
-        <Select value={cvSkill.name} disabled={true}>
-          <SelectTrigger className="text-muted-foreground">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value={cvSkill.name}>{cvSkill.name}</SelectItem>
-          </SelectContent>
-        </Select>
-      </Field>
-      <Field>
-        <Controller
-          control={control}
-          name="mastery"
-          render={({ field }) => (
-            <SkillMasterySelect
-              value={field.value}
-              onValueChange={field.onChange}
-            />
-          )}
-        />
-      </Field>
-    </FormDialog>
+    <SkillUpdateDialog {...formProps} skill={cvSkill}>
+      {children}
+    </SkillUpdateDialog>
   )
 }
